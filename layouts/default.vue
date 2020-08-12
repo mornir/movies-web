dc236d6a841eeab01652ebea3bae521f0cdc79ea
mornir <jeromepott@hotmail.com>
2020-08-12 20:26:07 +0200

    Fix DOM mistmach

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
-    <aside
-      v-if="$store.state.showDrafPreviewBanner"
-      class="sticky top-0 py-2 text-center bg-blue-500 bg-opacity-50"
-    >
-      <p class="font-semibold">
-        Draft Preview.
-        <button class="font-semibold underline" @click="$nuxt.refresh()">
-          Click here to refresh
-        </button>
-      </p>
-    </aside>
+    <client-only>
+      <aside
+        v-if="$store.state.showDrafPreviewBanner"
+        class="sticky top-0 py-2 text-center bg-blue-500 bg-opacity-50"
+      >
+        <p class="font-semibold">
+          Draft Preview.
+          <button class="font-semibold underline" @click="$nuxt.refresh()">
+            Click here to refresh
+          </button>
+        </p>
+      </aside>
+    </client-only>
     <div class="p-8 prose lg:prose-xl">
       <header>
         <h1><nuxt-link to="/">Home</nuxt-link></h1>
