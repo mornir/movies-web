31bc84567c007b0da7baeb3195a39c56fa6b8d95
mornir <jeromepott@hotmail.com>
2020-10-10 16:56:41 +0200

    Add external and internal link

---
+++ components/serializers/externalLink.vue
@@ -0,0 +1,11 @@
+<template functional>
+  <a
+    v-if="props.href && children[0] && children[0].text"
+    :href="props.href"
+    target="_blank"
+    rel="noopener"
+    class="underline"
+  >
+    <slot />
+  </a>
+</template>
---
+++ components/serializers/internalLink.vue
@@ -0,0 +1,9 @@
+<template functional>
+  <nuxt-link
+    v-if="props.slug && children[0] && children[0].text"
+    :to="props.slug === '/' ? '/' : `/${props.slug}/`"
+    class="underline"
+  >
+    <slot />
+  </nuxt-link>
+</template>
