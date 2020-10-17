7f78d513e7b2412520aed1016dfcdd6e4049ccc8
mornir <jeromepott@hotmail.com>
2020-10-17 20:24:39 +0200

    Add 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="p-8 mx-auto prose lg:prose-xl">
+    <div v-if="error.statusCode === 404">
+      <img :src="`https://http.cat/404.jpg`" />
+
+      <p>
+        Go back to
+        <nuxt-link to="/" class="underline text-primary">Home Page</nuxt-link>
+      </p>
+    </div>
+
+    <div v-else>
+      <h1>An error occurred</h1>
+      <p>Error Code: {{ error.statusCode }}</p>
+      <p>Error Message: {{ error.message }}</p>
+
+      <img :src="`https://http.cat/${error.statusCode}.jpg`" />
+      <nuxt-link to="/" class="underline text-primary">Back home</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  head() {
+    const { message, statusCode: code } = this.error
+    return {
+      title: `${code} – ${message}`,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: message,
+        },
+      ],
+    }
+  },
+}
+</script>
