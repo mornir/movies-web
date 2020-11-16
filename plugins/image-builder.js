862f86478e18730292b7f6127594705072bd33dd
mornir <jeromepott@hotmail.com>
2020-11-16 19:44:26 +0100

    Add auto format

--- plugins/image-builder.js
+++ plugins/image-builder.js
@@ -3,7 +3,7 @@ import imageUrlBuilder from '@sanity/image-url'
 export default ({ $sanity }, inject) => {
   const builder = imageUrlBuilder($sanity.config)
   function urlFor(source) {
-    return builder.image(source)
+    return builder.image(source).format('auto')
   }
   inject('urlFor', urlFor)
 }
