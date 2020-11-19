31ee288c40bb11eb90fa21147ad3f2e235c505db
mornir <jeromepott@hotmail.com>
2020-11-19 18:06:35 +0100

    Use webp

--- plugins/image-builder.js
+++ plugins/image-builder.js
@@ -3,7 +3,7 @@ import imageUrlBuilder from '@sanity/image-url'
 export default ({ $sanity }, inject) => {
   const builder = imageUrlBuilder($sanity.config)
   function urlFor(source) {
-    return builder.image(source).format('auto')
+    return builder.image(source).auto('format')
   }
   inject('urlFor', urlFor)
 }
