a5cabb8496749a10d2f2bd7a869387b93f1a405a
mornir <jeromepott@hotmail.com>
2020-12-05 16:55:48 +0100

    Add Cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,8 +14,9 @@ module.exports = {
     'prettier/vue',
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
+    'plugin:chai-friendly/recommended',
   ],
-  plugins: ['prettier', 'cypress'],
+  plugins: ['prettier', 'cypress', 'chai-friendly'],
   // add your custom rules here
   rules: {
     'no-console': 'off',
