cbc7ebf559e88cbceb52e6854e4d0a779bbd62dc
mornir <jeromepott@hotmail.com>
2021-04-04 14:01:04 +0200

    Remove color

--- pages/screenings/index.vue
+++ pages/screenings/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Screening Index 3</h1>
-    <p class="text-blue-700 underline">Today's screening</p>
+    <p>Today's screening</p>
     <p>
       <button type="button" class="focus:outline-none focus-visible:ring-2">
         Test focus-visible
